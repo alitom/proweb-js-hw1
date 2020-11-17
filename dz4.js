cd3a6f9ddf848142fc03e2068847e57b8c064b5c
Akram <73081278+alitom@users.noreply.github.com>
2020-11-18 00:31:30 +0500

    Update dz4.js

--- dz4.js
+++ dz4.js
@@ -9,3 +9,16 @@ let data = +prompt('Введите количество ступень : ');
      i++;
      symbol1 = symbol2+symbol1;
  }
+// for
+/*
+let data = +prompt('Введите количество ступень : ');
+ data = !isNaN(data) && data > 0 && data < 100 ? data : 5 ;
+ let symbol1 = prompt('Введите символ отступа : ') ;
+ let symbol2 = prompt('Введите конечный символ : ') ;
+ let i = 1; 
+ for(i=1; i <= data; i++ ){
+     console.log(symbol1)
+     
+     symbol1 = symbol2+symbol1;
+ }
+*/
